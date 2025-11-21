4517f0e0face6a3c522255312cc05f084d32a346
artofhunny <artofhunny@gmail.com>
2025-11-21 23:41:25 +0530

    changed something

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(
     credentials: true,
   })
 );
-app.options("*", cors());
+app.options(/.*/, cors());
 // app.use(cors({
 //     // origin: "http://localhost:5173",
 //     // origin: true,
